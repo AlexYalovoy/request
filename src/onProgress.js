d6a9f50236321ec33dc44b58daf356c8d7cc870a
AlexYalovoy <YalovyyAlex@gmail.com>
2019-01-29 17:38:45 +0200

    fix/review fix behavior of progress bar with miltiple download/upload

--- src/onProgress.js
+++ src/onProgress.js
@@ -1,33 +1,42 @@
 const onUpload = e => {
-  document.getElementById('uploadProgress')
-    .style.width = `${e.loaded * 100 / e.total}%`;
+  const uploadProgress = document.getElementById('uploadProgress');
+  uploadProgress.style.width = `${e.loaded * 100 / e.total}%`;
   const initTitle = document.title.match(/Request$/);
   document.title = `${parseInt(e.loaded * 100 / e.total, 10)}% ${initTitle}`;
 
+  if (e.loaded === e.total && e.total === 0) {
+    uploadProgress.style.opacity = 1;
+  }
+
   if (e.loaded === e.total && e.total !== 0) {
     setTimeout(() => {
-      document.getElementById('uploadProgress')
-        .style.opacity = 0;
-      document.getElementById('uploadProgress')
-        .style.width = 0;
+      uploadProgress.style.opacity = 0;
+      uploadProgress.style.width = 0;
       document.title = initTitle;
-    }, 1000);
+    }, 500);
+    setTimeout(() => {
+      uploadProgress.style.opacity = 1;
+      document.title = initTitle;
+    }, 1500);
   }
 };
 
-const onDownload = e => {
-  document.getElementById('downloadProgress')
-    .style.width = `${e.loaded * 100 / e.total}%`;
+function onDownload(e) {
+  const downloadProgress = document.getElementById('downloadProgress');
+  downloadProgress.style.width = `${e.loaded * 100 / e.total}%`;
   const initTitle = document.title.match(/Request$/);
   document.title = `${parseInt(e.loaded * 100 / e.total, 10)}% ${initTitle}`;
 
-  if (e.loaded === e.total && e.total !== 0) {
+  if (e.loaded === e.total && e.loaded !== 0) {
     setTimeout(() => {
-      document.getElementById('downloadProgress')
+      downloadProgress
         .style.opacity = 0;
-      document.getElementById('downloadProgress')
-        .style.width = 0;
+      downloadProgress.style.width = 0;
       document.title = initTitle;
-    }, 1000);
+    }, 500);
+    setTimeout(() => {
+      downloadProgress.style.opacity = 1;
+      document.title = initTitle;
+    }, 1500);
   }
-};
+}
