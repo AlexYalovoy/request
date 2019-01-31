6e331273aa256252fd7da38bb2ee24921d3351b1
AlexYalovoy <YalovyyAlex@gmail.com>
2019-01-31 14:30:56 +0200

    fix/fileStructure edit onProgress.js

--- src/onProgress.js
+++ src/onProgress.js
@@ -1,42 +1,29 @@
-const onUpload = e => {
-  const uploadProgress = document.getElementById('uploadProgress');
-  uploadProgress.style.width = `${e.loaded * 100 / e.total}%`;
-  const initTitle = document.title.match(/Request$/);
-  document.title = `${parseInt(e.loaded * 100 / e.total, 10)}% ${initTitle}`;
+/* eslint-disable wrap-iife */
+(function() {
+  const initTitle = document.title;
 
-  if (e.loaded === e.total && e.total === 0) {
-    uploadProgress.style.opacity = 1;
+  function onProgress(node) {
+    return e => {
+      node.style.width = `${e.loaded * 100 / e.total}%`;
+      document.title = `${parseInt(e.loaded * 100 / e.total, 10)}% ${initTitle}`;
+
+      if (e.loaded === e.total && e.total === 0) {
+        node.style.opacity = 1;
+      }
+
+      if (e.loaded === e.total && e.total !== 0) {
+        setTimeout(() => {
+          node.style.opacity = 0;
+          node.style.width = 0;
+          document.title = initTitle;
+        }, 500);
+        setTimeout(() => {
+          node.style.opacity = 1;
+        }, 1500);
+      }
+    };
   }
 
-  if (e.loaded === e.total && e.total !== 0) {
-    setTimeout(() => {
-      uploadProgress.style.opacity = 0;
-      uploadProgress.style.width = 0;
-      document.title = initTitle;
-    }, 500);
-    setTimeout(() => {
-      uploadProgress.style.opacity = 1;
-      document.title = initTitle;
-    }, 1500);
-  }
-};
-
-function onDownload(e) {
-  const downloadProgress = document.getElementById('downloadProgress');
-  downloadProgress.style.width = `${e.loaded * 100 / e.total}%`;
-  const initTitle = document.title.match(/Request$/);
-  document.title = `${parseInt(e.loaded * 100 / e.total, 10)}% ${initTitle}`;
-
-  if (e.loaded === e.total && e.loaded !== 0) {
-    setTimeout(() => {
-      downloadProgress
-        .style.opacity = 0;
-      downloadProgress.style.width = 0;
-      document.title = initTitle;
-    }, 500);
-    setTimeout(() => {
-      downloadProgress.style.opacity = 1;
-      document.title = initTitle;
-    }, 1500);
-  }
-}
+  window.onUpload = onProgress(document.getElementById('uploadProgress'));
+  window.onDownload = onProgress(document.getElementById('downloadProgress'));
+})();
